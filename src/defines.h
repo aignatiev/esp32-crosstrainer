f02ebcc74289e78a176509b7644c61ae34f33591
Alexandre Ignatiev <alexandre.ignatiev@gmail.com>
2023-01-08 17:07:26 +0200

    Moved private stuff to a separate file

--- src/defines.h
+++ src/defines.h
@@ -1,16 +1,13 @@
 #ifndef DEFINES_H
 #define DEFINES_H
 
-#define WIFI_SSID       "ssid"
-#define WIFI_PASSWORD   "pwd"
+#include "private.h"
 
 // Stuff related to the Google Script
-#define API_TOKEN "xyz"
 #define WEB_SERVER "script.google.com"
 #define WEB_URL "https://script.google.com/macros/s/" API_TOKEN "/exec?%s"
 
 // Stuff related to OTA
-#define OTA_TOKEN "xyz"
 #define OTA_URL "https://drive.google.com/uc?export=download&id=" OTA_TOKEN
 
 // This one defines whether there is any network stuff included (for debugging ULP)
---
+++ src/private.h
@@ -0,0 +1,19 @@
+#ifndef PRIVATE_H
+#define PRIVATE_H
+
+// File for private settings, run the following command after editing
+// git update-index --assume-unchanged src/private.h
+// To revert run the following command
+// git update-index --no-assume-unchanged src/private.h
+
+// Hard-coded WiFi settings
+#define WIFI_SSID       "ssid"
+#define WIFI_PASSWORD   "pwd"
+
+// ID of the Google Script
+#define API_TOKEN "asd"
+
+// ID of the OTA file in Google Drive
+#define OTA_TOKEN "jkl"
+
+#endif
